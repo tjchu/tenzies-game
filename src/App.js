88e51ed0354338c69e4be19225188923d4fd63c5
Terry Chu <terrychu2002@gmail.com>
2023-11-02 21:55:46 -0700

    update

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ TODO features:
 function App() {
   const diceAmount = 10;
   const [dice, setDice] = React.useState(allNewDice());
+  const [rolls, setRolls] = React.useState(0);
   const [tenzies, setTenzies] = React.useState(false);
 
   React.useEffect(() => {
@@ -48,10 +49,12 @@ function App() {
   function rollDice() {
     if (tenzies) {
       setDice(allNewDice());
+      setRolls(0);
       setTenzies(false);
     }
 
     else {
+      setRolls(prevRolls => prevRolls + 1);
       setDice(
         prevDice => prevDice.map(die => !die.isHeld ? {...die, value: getRandomNum()} : die)
       );
@@ -59,9 +62,10 @@ function App() {
   }
 
   function holdDice(id) {
-    setDice(
-      prevDice => prevDice.map(die => die.id === id ? {...die, isHeld: !die.isHeld} : die)
-    );
+    if (!tenzies)
+      setDice(
+        prevDice => prevDice.map(die => die.id === id ? {...die, isHeld: !die.isHeld} : die)
+      );
   }
 
   const diceElements = dice.map(die => <Die key={die.id} value={die.value} id={die.id} isHeld={die.isHeld} holdFunc={holdDice} />);
@@ -70,10 +74,11 @@ function App() {
     <main>
       {tenzies && <Confetti/>}
       <h1 className="title">Tenzies</h1>
-      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <p className="instructions">{tenzies ? `Congratulations! It took you ${rolls} rolls to win.` : "Roll until all dice are the same. Click each die to freeze it at its current value between rolls."}</p>
       <div className="dice-container">
         {diceElements}
       </div>
+
       <button className="roll-button" onClick={rollDice}>
         {tenzies ? "New Game" : "Roll"}
       </button>
