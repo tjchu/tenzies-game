80dcb5c1c087afc2180263dc6fabf1b81c0aa7ac
Terry Chu <terrychu2002@gmail.com>
2023-11-22 23:27:42 +0800

    update

--- src/Die.js
+++ src/Die.js
@@ -12,11 +12,19 @@ function Die(props) {
 
 	return (
 		<button className="die" style={styles} onClick={handleClick}>
-			<div className="die-value">
-				{props.isEmpty ?  "" : props.value}
+			<div class="face">
+			    <span className="pip">.</span>
+			    <span className="pip">.</span>
+			    <span className="pip">.</span>
 			</div>
 		</button>
 	);
 }
+
+/*
+			<div className="die-value">
+				{props.isEmpty ?  "" : props.value}
+			</div>
+*/
 
 export default Die;
