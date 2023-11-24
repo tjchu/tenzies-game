f22cc23c3c5d65ad1e334ea35ede1d4311ce38f5
Terry Chu <terrychu2002@gmail.com>
2023-11-24 00:27:48 -0800

    dice dots feature (#2)
    * dice dots feature
    
    * update

--- src/Die.js
+++ src/Die.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {nanoid} from "nanoid"
 
 
 function Die(props) {
@@ -10,13 +11,27 @@ function Die(props) {
 		props.holdFunc(props.id);
 	}
 
+
+	// Utilizing css grid layout, each specific dice dot is laid out in the right position according to the CSS
+	const pips = []
+
+	for (let i = 0; i < props.value; i++) {
+		pips.push(<div key={nanoid()} className="dot"></div>);
+	}
+
 	return (
 		<button className="die" style={styles} onClick={handleClick}>
-			<div className="die-value">
-				{props.isEmpty ?  "" : props.value}
+			<div class="face">
+				{props.isEmpty ?  "" : pips}
 			</div>
 		</button>
 	);
 }
+
+/*
+<div className="die-value">
+	{props.isEmpty ?  "" : props.value}
+</div>
+*/
 
 export default Die;
